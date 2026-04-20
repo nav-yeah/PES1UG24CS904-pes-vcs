601919c3e8be312d4ad13289710a864f077c1d46
unknown <nav-yeah@users.noreply.github.com>
2026-04-20 20:45:55 +0530

    Phase 4: Serialize commit object with tree, parent, author, message

--- commit.c
+++ commit.c
@@ -6,9 +6,7 @@
 //   parent <64-char-hex-hash>        ← omitted for the first commit
 //   author <name> <unix-timestamp>
 //   committer <name> <unix-timestamp>
-//
-//   <commit message>
-//
+
 
 
 #include "commit.h"
